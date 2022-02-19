fc025edb0b23b55b7d8286ff40633d141d58815d
mahmoud-47 <77865928+mahmoud-47@users.noreply.github.com>
2022-02-19 18:32:42 +0000

    Update settings.py

--- BusProjet/settings.py
+++ BusProjet/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside the projet like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -128,7 +128,7 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'gysoft.sn@gmail.com'
-EMAIL_HOST_PASSWORD = 'allaHOU2001'
+EMAIL_HOST_USER = '****@***'
+EMAIL_HOST_PASSWORD = '*****'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
